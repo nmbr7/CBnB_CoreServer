0e2ec8e5805ab50a93f8219ea814f0cb033a2168
nmbr7 <suhailr7@ymail.com>
2020-03-05 01:26:54 +0530

    update

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -13,20 +13,3 @@ table! {
         cpu_model -> Varchar,
     }
 }
-
-table! {
-    Node_state (node_id) {
-        node_id -> Varchar,
-        uptime -> Varchar,
-        power_mode -> Varchar,
-    }
-}
-
-table! {
-    Nodes (id) {
-        id -> Varchar,
-        ip -> Varchar,
-    }
-}
-
-allow_tables_to_appear_in_same_query!(Node_resources, Node_state, Nodes,);
